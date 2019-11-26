374421f414470c2bbc1800b3e45443a6556fc17c
Yan_Badea <yan_badea@yahoo.com>
2019-11-26 23:14:13 +0000

    Add files via upload

---
+++ src/OOTrainer.java
@@ -0,0 +1,15 @@
+public class OOTrainer extends Teacher {
+    /**
+     * Initializes the Instructor, takes a string, char and int as parameter which will be the name, gender and age.
+     */
+    public OOTrainer(String name, char gender, int age) {
+        super(name, gender, age);
+    }
+
+    /**
+     *Returns whether the OOTrainer can teach the given subject, depending on the subject's level of specialism.
+     */
+    public Boolean canTeach(Subject subject) {
+        return subject.getSpecialism() <= 3;
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+/**
+ * Student, subclass to the Person class. It also contains a list of certificates which is defined by an arraylist.
+ */
+public class Student extends Person {
+
+    //A list of subject IDs that the student currently has.
+    private ArrayList<Integer> certificates = new ArrayList<Integer>();
+
+    /**
+     * Creates the student object, asks for the same input as the person class along with a list of IDs.
+     */
+    public Student ( String name, char gender, int age ) {
+        super (name, gender, age);
+    }
+
+    /**
+     * Adds the ID of the subject to the collections of certificates.
+     */
+    public void graduate ( Subject subject ) {
+        certificates.add(subject.getID());
+    }
+
+    /**
+     *  Returns the ArrayList of certificates obtained by the students.
+     */
+    public ArrayList<Integer> getCertificates() {
+        return certificates;
+    }
+
+    /**
+     * Checks whether or not the student has already obtained the certificate for the input subject.
+     * Returns true if it does, otherwise it returns false.
+     */
+    public boolean hasCertificate( Subject subject ) {
+
+        for (int ID : certificates) {
+            if (ID == subject.getID()) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
