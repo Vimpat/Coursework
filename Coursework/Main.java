249cc857d4e8fa4b3fd3fc219fb8e5c435d0a5dd
Yan <yan_badea@yahoo.com>
2019-12-26 20:44:06 +0200

    Add more subject objects

--- Coursework/Main.java
+++ Coursework/Main.java
@@ -5,6 +5,12 @@ public class Main {
         Subject subject1 = new Subject(1, 2, 3);
         Subject subject2 = new Subject(2, 3, 4);
         Subject subject3 = new Subject(2, 3, 4);
+        Subject subject3 = new Subject(2, 3, 4);
+        Subject subject3 = new Subject(2, 3, 4);
+        Subject subject3 = new Subject(2, 3, 4);
+        Subject subject3 = new Subject(2, 3, 4);
+        Subject subject3 = new Subject(2, 3, 4);
+        Subject subject3 = new Subject(2, 3, 4);
         Course course1 = new Course(subject1, 2);
         Student student1 = new Student("Alice", 'F', 25);
         Student student2 = new Student("Bob", 'M', 30);
